2455f3334d713542e85699db2013cbf945785c74
Laura Nicolas <laura.nicolas@es.logicalis.com>
2017-09-28 05:39:23 -0400

    OK

--- menu.php
+++ menu.php
@@ -11,7 +11,7 @@
       <table> 
         <tr>
           <td width=100px>
-           <a href="load.php">Load Test</a>
+           <a href="load.php">Load CPU Test</a>
           </td>
           <td width=100px>
             <a href="load2.php">Stop Test</a>
