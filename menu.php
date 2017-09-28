26cb5150c9ee210bed3740f43ebf7f8274e56066
Laura Nicolas <laura.nicolas@es.logicalis.com>
2017-09-28 11:55:14 -0400

    OK

--- menu.php
+++ menu.php
@@ -1,8 +1,6 @@
 <nav class="navbar navbar-default" role="navigation">
+<img src="logicalis-logo.svg" alt="Logicalis - business and technology working as one" style="max-width: 200%;" width="125%">
 
-<div class="navbar-header">
-  <a class="navbar-brand" href="/"><img width="75" src="Logicalis_logo.png" /></a>
-</div>
 
 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
   <ul class="nav navbar-nav">
