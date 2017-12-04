8ddf84fc9fdb0fa7c58db0253c2661ac5ca99db1
Laura Nicolas <laura.nicolas@es.logicalis.com>
2017-12-04 06:02:31 -0500

    OK

--- menu.php
+++ menu.php
@@ -23,7 +23,7 @@
             <a href="edit.php">Insert Data    </a>
           </td>
           <td width=400px>
-             <p align="right"> <font size="4" color="black" > OPENSHIFT 2017</font></p>
+             <p align="right"> <font size="4" color="black" > 2017</font></p>
           </td>
         </tr>
       </table>
